eb137b317c066db4e7b10ec8e4b75b4331c669c3
Joseph Cheverton-Wynne <joseph.cheverton-wynne@bjss.com>
2020-10-22 09:32:31 +0100

    fixed leaky broken goroutines

--- pkg/tcpserver/tcpserver.go
+++ pkg/tcpserver/tcpserver.go
@@ -1,76 +1,72 @@
 package tcpserver
 
 import (
-    "bufio"
-    "fmt"
-    "net"
-    "sync"
-    "tcpspeeddating/pkg/chatroom"
+	"bufio"
+	"net"
+	"sync"
+	"tcpspeeddating/pkg/chatroom"
 )
 
 var (
-    connections = make(map[chan []byte]struct{})
-    mu = new(sync.Mutex)
+	connections = make(map[chan []byte]struct{})
+	mu          = new(sync.Mutex)
 )
 
-
-
 func Run() error {
-    listen, err := net.Listen("tcp", ":8000")
-    if err != nil {
-        return err
-    }
+	listen, err := net.Listen("tcp", ":8000")
+	if err != nil {
+		return err
+	}
 
-    for {
-        conn, err := listen.Accept()
-        if err != nil {
-            break
-        }
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			break
+		}
 
-        go handleConn(conn)
-    }
+		go handleConn(conn)
+	}
 
-    return nil
+	return nil
 }
 
 func writer(conn net.Conn, messages chan []byte, done chan struct{}) {
-    for {
-        select {
-        case m := <-messages:
-            conn.Write(append(m, '\n'))
-        case <-done:
-            return
-        }
-    }
+	for {
+		select {
+		case m := <-messages:
+			conn.Write(append(m, '\n'))
+		case <-done:
+			return
+		}
+	}
 }
 
 func reader(conn net.Conn, msgSend chan []byte, done chan struct{}) {
-    scanner := bufio.NewScanner(conn)
-    for scanner.Scan() {
-        msgSend<- scanner.Bytes()
-    }
-    close(done)
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		msgSend <- scanner.Bytes()
+	}
+	close(done)
 }
 
 func handleConn(conn net.Conn) {
-    msgRecv := make(chan []byte)
-    msgSend := make(chan []byte)
-    done := make(chan struct{})
+	msgRecv := make(chan []byte)
+	msgSend := make(chan []byte)
+	done := make(chan struct{})
 
-    mu.Lock()
-    connections[msgRecv] = struct{}{}
-    mu.Unlock()
-    defer func() {
-        mu.Lock()
-        delete(connections, msgRecv)
-        mu.Unlock()
-    }()
+	mu.Lock()
+	connections[msgRecv] = struct{}{}
+	mu.Unlock()
+	defer func() {
+		mu.Lock()
+		delete(connections, msgRecv)
+		mu.Unlock()
+	}()
 
-    go writer(conn, msgRecv, done)
-    go reader(conn, msgSend, done)
-    chans := chatroom.Chans{In: msgSend, Out: msgRecv}
-    chatroom.AddToPool(chans)
-    <-done
-    chatroom.Remove(chans)
-    fmt.Println("12345555")
+	go writer(conn, msgRecv, done)
+	go reader(conn, msgSend, done)
+	chans := chatroom.Chans{In: msgSend, Out: msgRecv}
+	chatroom.AddToPool(chans)
+	<-done
+	chatroom.Remove(chans)
 }
